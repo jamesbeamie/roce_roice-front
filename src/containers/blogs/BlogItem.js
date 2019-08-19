3c8ec3a3a3c4765400e6b6894fc407476b1bd237
jamesbeamie <wafulajames9@gmail.com>
2019-08-19 15:15:11 +0300

    added functionality to fetch Blogs

---
+++ src/containers/blogs/BlogItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import '../../assets/styling/blogItem.css';
+import '../../assets/styles/blogItem.css';
+
+const BlogItem = (props) => (
+	<li key={props.blogId} className="home_list-item">
+		<div>
+			<h3>Title: {props.title}</h3>
+			<h3>Description: {props.description}</h3>
+			<h3>Tag: {props.tag}</h3>
+		</div>
+		<div>
+			<React.Fragment>
+				<p>owner</p>
+				<button className="btn">View home</button>
+			</React.Fragment>
+		</div>
+	</li>
+);
+
+export default BlogItem;
---
+++ src/containers/blogs/BlogList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import BlogItem from './BlogItem';
+import '../../assets/styles/blogList.css';
+
+const BlogList = (props) => {
+	// can also use () then call this.props
+	const fetchedBlogs = props.blogs.map((blog) => {
+		console.log('blogths',props.blogs);
+		return (
+			<BlogItem
+				key={blog._id}
+				blogId={blog._id}
+				title={blog.name}
+                description={blog.price}
+                tag={blog.tag}
+			/>
+		);
+	});
+	return <ul className="home_list">{fetchedBlogs}</ul>;
+};
+
+export default BlogList;
