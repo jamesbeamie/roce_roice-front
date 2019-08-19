d7d2e7b5eca984a96d67f918d1c1d82432304aef
jamesbeamie <wafulajames9@gmail.com>
2019-08-19 16:24:47 +0300

    updated the modal with a spinner

--- src/containers/createBlog.js
+++ src/containers/createBlog.js
@@ -4,18 +4,31 @@ import '../assets/styles/blog.css';
 import Modal from '../common/modal';
 import Backdrop from '../common/backdrop';
 import BlogList from './blogs/BlogList';
+import loginContext from '../common/loginContext';
+import Spinner from '../common/Spinner';
 
 class CreateBlog extends Component {
 	state = {
 		creating: false,
-		blogArray: []
+		blogArray: [],
+		isLoading: false
 	};
+
+	constructor(props) {
+		super(props);
+
+		this.titleEl = React.createRef();
+		this.descriptionEl = React.createRef();
+		this.tagEl = React.createRef();
+	}
 
 	componentDidMount() {
 		this.fetchBlogs();
 	}
 
-	handleCreateHome = () => {
+	static contextType = loginContext;
+
+	handleCreateBlog = () => {
 		this.setState({
 			creating: true
 		});
@@ -31,6 +44,65 @@ class CreateBlog extends Component {
 		this.setState({
 			creating: false
 		});
+		const title = this.titleEl.current.value;
+		const description = this.descriptionEl.current.value;
+		const tag = this.tagEl.current.value;
+
+		// validation
+
+		if (title.trim().length === 0 || description.trim().length === 0 || tag.trim().length === 0) {
+			return;
+		}
+		const blog = { title, description, tag };
+		console.log('newBlog', blog);
+		const requestBody = {
+			query: `
+                mutation {
+                    createBlog(blogInput: {title: "${title}", description: "${description}", tag: "${tag}"}){
+                        _id
+   						title
+    					description
+    					tag
+                    }
+                }
+            `
+		};
+
+		// get token from context
+
+		const token = this.context.token;
+		// acces api
+		fetch('https://royalframes-photography.herokuapp.com/photography', {
+			method: 'POST',
+			body: JSON.stringify(requestBody),
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + token
+			}
+		})
+			.then((res) => {
+				if (res.status !== 200 && res.status !== 201) {
+					throw new Error('Error creating Blog');
+				}
+				return res.json();
+			})
+			.then((resData) => {
+				console.log('resData', resData);
+				// this.fetchHomes();
+				this.setState((prevState) => {
+					const updatedArray = [ ...prevState.blogArray ];
+					updatedArray.push({
+						_id: resData.data.createBlog._id,
+						title: resData.data.createBlog.title,
+						description: resData.data.createBlog.description,
+						tag: resData.data.createBlog.tag
+					});
+					return { blogArray: updatedArray };
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 
 	fetchBlogs = () => {
@@ -66,16 +138,20 @@ class CreateBlog extends Component {
 				console.log('fetchedData', resData);
 				const blogs = resData.data.blogs;
 				this.setState({
-					blogArray: blogs
+					blogArray: blogs,
+					isLoading: false
 				});
 			})
 			.catch((err) => {
 				console.log(err);
+				this.setState({
+					isLoading: false
+				});
 			});
 	};
 
 	render() {
-		const { creating, blogArray } = this.state;
+		const { creating, blogArray, isLoading } = this.state;
 		return (
 			<React.Fragment>
 				{creating && <Backdrop />}
@@ -87,14 +163,27 @@ class CreateBlog extends Component {
 						onCancel={this.handleCancel}
 						onConfirm={this.handleConfirm}
 					>
-						<p>Blog modal</p>
+						<form>
+							<div className="form-control">
+								<label htmlFor="title">Title</label>
+								<input type="text" id="title" ref={this.titleEl} />
+							</div>
+							<div className="form-control">
+								<label htmlFor="description">Description</label>
+								<input type="text" id="description" ref={this.descriptionEl} />
+							</div>
+							<div className="form-control">
+								<label htmlFor="tag">Tag</label>
+								<input type="text" id="tag" ref={this.tagEl} />
+							</div>
+						</form>
 					</Modal>
 				)}
 				<div className="home-control">
 					<h4>Create Blog</h4>
-					<button onClick={this.handleCreateHome}> Click to create</button>
+					<button onClick={this.handleCreateBlog}> Click to create</button>
 				</div>
-				<BlogList blogs={blogArray} />
+				{isLoading ? <Spinner /> : <BlogList blogs={blogArray} />}
 			</React.Fragment>
 		);
 	}
